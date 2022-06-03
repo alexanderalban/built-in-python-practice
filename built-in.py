c47d17a08e624d703209d25416282d4a76ae7dc0
alexanderalban <albanalexander@gmail.com>
2022-06-02 21:24:22 -0500

    added dir, eval, exec, and float

--- built-in.py
+++ built-in.py
@@ -18,3 +18,37 @@ print(bool(None))
 print(bool(' '))
 print(bool("Hello"))
 
+# The dir function, short for directory, returns information about any value
+
+print(dir(['a', 'short', 'list']))
+print(dir(1))
+
+popcorn = 'I love popcorn!'
+print(dir(popcorn))
+
+help(popcorn.upper)
+
+# Eval, short for evaluate. Takes a string as a parameter and runs it as though it were a Python expression
+
+# your_calculation = input('Enter a calculation')
+# result = eval(your_calculation)
+# print(result)
+
+# The exec function is like eval, but does not return a value. Can be used to run more complicated programs
+
+my_small_program = '''print('ham')
+print('sandwich')'''
+
+exec(my_small_program)
+
+# The float function, turns an integer (10, 34, etc) into a floating point number (10.0, 34.0, etc)
+
+print(float('12'))
+
+#Int turns an integer or string into a whole number
+
+print(int(123.45))
+print(int('456'))
+
+# A string containing a floating point number doens't work. It's confusing
+print(int('123.456'))
